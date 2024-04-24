56f2a423e4dff87a9705dd46853e1c734e209c3b
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-04-24 16:17:32 +0300

    [Product-Service] base of service

---
+++ product-service/src/test/kotlin/ru/kafka/productservice/ProductServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.kafka.productservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
